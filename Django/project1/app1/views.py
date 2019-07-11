272da5150684a654f50733f1085d39f36b316051
RobStepanyan <robert1stepanyan@gmail.com>
2019-07-11 17:06:04 +0400

    Update

--- Django/project1/app1/views.py
+++ Django/project1/app1/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>This is a fuckin home page of app1</h1>')
+    return render(request, 'app1_templates/home.html')
 
 def about(request):
-    return HttpResponse('<h1>And this is its fuckin brother about page</h1>')
+    return render(request, 'app1_templates/about.html')
