169ff8880146e509f7d213dad9c484ab02e331aa
RobStepanyan <robert1stepanyan@gmail.com>
2019-07-11 17:34:26 +0400

    Update

--- Django/project1/app1/views.py
+++ Django/project1/app1/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+    {
+        "author": "Robert",
+        "title": "First post",
+        "content": "First post's content",
+        "date_posted": "11-Jul-2019"
+    },
+    {
+        "author": "John",
+        "title": "Second post",
+        "content": "Second post's content",
+        "date_posted": "11-Jul-2019"
+    },
+]
+
+
 def home(request):
-    return render(request, 'app1_templates/home.html')
+    return render(request, 'app1_templates/home.html', {'title': 'App1 - Home Page', 'posts': posts})
 
 def about(request):
-    return render(request, 'app1_templates/about.html')
+    return render(request, 'app1_templates/about.html', {'title': 'App1 - About Page'})
