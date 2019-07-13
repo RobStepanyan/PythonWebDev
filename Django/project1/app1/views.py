a098cabb5ba56276478a85288ea53c9515c0b105
RobStepanyan <robert1stepanyan@gmail.com>
2019-07-13 18:46:12 +0400

    Database and migrations

--- Django/project1/app1/views.py
+++ Django/project1/app1/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
-posts = [
-    {
-        "author": "Robert",
-        "title": "First post",
-        "content": "First post's content",
-        "date_posted": "11-Jul-2019"
-    },
-    {
-        "author": "John",
-        "title": "Second post",
-        "content": "Second post's content",
-        "date_posted": "11-Jul-2019"
-    },
-]
-
-
 def home(request):
-    return render(request, 'app1_templates/home.html', {'title': 'App1 - Home Page', 'posts': posts})
+    return render(request, 'app1_templates/home.html', {'title': 'App1 - Home Page', 'posts': Post.objects.all()})
 
 def about(request):
     return render(request, 'app1_templates/about.html', {'title': 'App1 - About Page'})
