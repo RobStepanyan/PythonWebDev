d589602929a1bb9c7b6715f6527a5e84c6cd37e7
RobStepanyan <robert1stepanyan@gmail.com>
2019-08-10 16:16:48 +0400

    Finished Profile Updating

--- Django/project2/users/views.py
+++ Django/project2/users/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-
-from .forms import UserSignUpForm
+from .forms import UserSignUpForm, UserUpdateForm, ProfileUpdateForm
 
 
 def sign_up(request):
@@ -25,7 +24,23 @@ def sign_up(request):
 
 @login_required
 def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm( request.POST, request.FILES, 
+                                    instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    
     context = {
         'title': 'Profile',
+        'u_form': u_form,
+        'p_form': p_form,
     }
     return render(request, 'users/profile.html', context)
