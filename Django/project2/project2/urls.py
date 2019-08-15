1c03dbe47d5a2373ea97f297a4d685f8c387c943
RobStepanyan <robert1stepanyan@gmail.com>
2019-08-15 17:06:53 +0400

    Email password reset

--- Django/project2/project2/urls.py
+++ Django/project2/project2/urls.py
@@ -27,7 +27,16 @@ urlpatterns = [
     path('signup/', user_views.sign_up, name='sign_up'),
     path('login/', auth_views.LoginView.as_view(template_name='users/log_in.html'), name='log_in'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/log_out.html'), name='log_out'),
-    path('profile/', user_views.profile, name='profile')
+    path('profile/', user_views.profile, name='profile'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='users/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/<uidb64>/<token>/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete.html'), name='password_reset_complete.html'),
+    
 ]
 
 if settings.DEBUG:
