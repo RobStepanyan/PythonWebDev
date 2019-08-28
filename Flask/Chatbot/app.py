b5553bfaaeac96c448811fb1ffc2bccf4f956146
RobStepanyan <robert1stepanyan@gmail.com>
2019-08-28 15:47:34 +0400

    Chatbot: Connected to MongoDB Atlas

--- Flask/Chatbot/app.py
+++ Flask/Chatbot/app.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
-
+import os
 
 app = Flask(__name__)
 
-
-chatbot = ChatBot('Ron Obvious')
+MONGODB_PASS = os.environ.get('mongoDBPASS')
+chatbot = ChatBot(
+    'Ron Obvious', 
+    storage_adapter='chatterbot.storage.MongoDatabaseAdapter', 
+    database_uri='mongodb+srv://admin:'+ MONGODB_PASS +'@cluster0-62hvi.gcp.mongodb.net/test?retryWrites=true&w=majority')
 
 # Create a new trainer for the chatbot
 trainer = ChatterBotCorpusTrainer(chatbot)
 
 # Train the chatbot based on the english corpus
-trainer.train("chatterbot.corpus.english")
+trainer.train('chatterbot.corpus.english')
 
 
 @app.route('/')
@@ -26,5 +29,5 @@ def get_bot_response():
     return str(chatbot.get_response(userText))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
